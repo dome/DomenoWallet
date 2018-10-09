03f01357afa2ebc6a75eca5cb21574cbe129a8ff
ety001 <ety001@domyself.me>
2018-10-09 21:17:19 +0800

    add server cache for trade pairs

--- server/index.js
+++ server/index.js
@@ -58,145 +58,145 @@ server.route({
   }
 });
 
+const getTradePair = async (pointPair) => {
+  const allPromises = []
+  TRADE_PAIRS.forEach(pair => {
+    allPromises.push(
+      StellarServer
+        .tradeAggregation(pair.from, pair.to, null, null, 86400000)
+        .limit(1)
+        .order('desc')
+        .call()
+        .then(res => {
+          return {type: 'trade_aggregation', pair: pair, records: res.records}
+        })
+    )
+    allPromises.push(
+      StellarServer
+        .trades()
+        .forAssetPair(pair.from, pair.to)
+        .order('desc')
+        .limit(1)
+        .call()
+        .then(res => {
+          return {type: 'trades', pair: pair, records: res.records}
+        })
+    )
+  })
+  return Promise.all(allPromises).then(resp => {
+    const tradeDicts = []
+    resp.forEach(res => {
+      if (res.type === 'trades') {
+        if (res.records.length > 0) {
+          tradeDicts.push(res.records[0])
+        }
+      }
+    })
+
+    const allResp = {XCN:[]}
+    resp.forEach(result => {
+      if (result.type === 'trades') return
+      const pair = result.pair
+      const records = result.records
+      const tmp = {}
+      if (pair.from.isNative()) {
+        tmp['base_asset'] = {
+          'code': pair.from.code,
+          'issuer': null
+        }
+      } else {
+        tmp['base_asset'] = {
+          'code': pair.from.code,
+          'issuer': pair.from.issuer
+        }
+      }
+      if (pair.to.isNative()) {
+        tmp['counter_asset'] = {
+          'code': pair.to.code,
+          'issuer': null
+        }
+      } else {
+        tmp['counter_asset'] = {
+          'code': pair.to.code,
+          'issuer': pair.to.issuer
+        }
+      }
+
+      if (records.length > 0) {
+        tmp['stat'] = {
+          "low": records[0].low,
+          "base_volume": records[0].base_volume,
+          "close": records[0].close,
+          "order_book_avg": records[0].avg,
+          "counter_volume": records[0].counter_volume,
+          "high": records[0].high,
+          "open": records[0].open
+        }
+        tradeDicts.forEach(trade => {
+          if (pair.from.isNative()) {
+            if (trade.base_asset_type === 'native' && trade.counter_asset_code === pair.to.code && trade.counter_asset_issuer === pair.to.issuer) {
+              tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
+              tmp['stat']['latest_price'] = trade.price.n / trade.price.d
+            }
+          } else if (pair.to.isNative()) {
+            if (trade.counter_asset_type === 'native' && trade.base_asset_code === pair.from.code && trade.base_asset_issuer === pair.from.issuer) {
+              tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
+              tmp['stat']['latest_price'] = trade.price.n / trade.price.d
+            }
+          } else {
+            if (trade.counter_asset_code === pair.to.code && trade.counter_asset_issuer === pair.to.issuer && trade.base_asset_code === pair.from.code && trade.base_asset_issuer === pair.from.issuer) {
+              tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
+              tmp['stat']['latest_price'] = trade.price.n / trade.price.d
+            }
+          }
+        })
+
+      } else {
+        tmp['stat'] = {
+          "latest_price_xcn": "0",
+          "low": "0",
+          "base_volume": "0",
+          "close": "0",
+          "order_book_avg": "0",
+          "latest_price": "0",
+          "counter_volume": "0",
+          "high": "0",
+          "open": "0"
+        }
+      }
+      if (allResp[pair.to.code] == undefined) {
+        allResp[pair.to.code] = []
+      }
+      allResp[pair.to.code].push(tmp)
+    })
+    return allResp
+  })
+  .catch(err => {
+    return err
+  })
+}
+
+const traidPairCache = server.cache({
+  expiresIn: 10 * 1000,
+  segment: 'customSegment',
+  generateFunc: async (pair) => {
+    return await getTradePair(pair);
+  },
+  generateTimeout: 20000
+});
+
 server.route({
   method: 'GET',
   path: '/v2/api/trade_aggregations',
   options: {
     cors: true
   },
-  handler: (request, h) => {
-    let timeout = 5000
+  handler: async (request, h) => {
+    let pointTime = new Date().getTime()
     if (request.query.r) {
-      timeout = request.query.r
+      pointTime = request.query.r
     }
-    const allPromises = []
-    TRADE_PAIRS.forEach(pair => {
-      allPromises.push(
-        StellarServer
-          .tradeAggregation(pair.from, pair.to, null, null, 3600000)
-          .limit(1)
-          .order('desc')
-          .call()
-          .then(res => {
-            return {type: 'trade_aggregation', pair: pair, records: res.records}
-          })
-      )
-      allPromises.push(
-        StellarServer
-          .trades()
-          .forAssetPair(pair.from, pair.to)
-          .order('desc')
-          .limit(1)
-          .call()
-          .then(res => {
-            return {type: 'trades', pair: pair, records: res.records}
-          })
-      )
-    })
-    return Promise.all(allPromises).then(resp => {
-      const tradeDicts = []
-      resp.forEach(res => {
-        if (res.type === 'trades') {
-          if (res.records.length > 0) {
-            tradeDicts.push(res.records[0])
-          }
-        }
-      })
-
-      const allResp = {XCN:[]}
-      resp.forEach(result => {
-        if (result.type === 'trades') return
-        const pair = result.pair
-        const records = result.records
-        const tmp = {}
-        if (pair.from.isNative()) {
-          tmp['base_asset'] = {
-            'code': pair.from.code,
-            'issuer': null
-          }
-        } else {
-          tmp['base_asset'] = {
-            'code': pair.from.code,
-            'issuer': pair.from.issuer
-          }
-        }
-        if (pair.to.isNative()) {
-          tmp['counter_asset'] = {
-            'code': pair.to.code,
-            'issuer': null
-          }
-        } else {
-          tmp['counter_asset'] = {
-            'code': pair.to.code,
-            'issuer': pair.to.issuer
-          }
-        }
-
-        if (records.length > 0) {
-          tmp['stat'] = {
-            "low": records[0].low,
-            "base_volume": records[0].base_volume,
-            "close": records[0].close,
-            "order_book_avg": records[0].avg,
-            "counter_volume": records[0].counter_volume,
-            "high": records[0].high,
-            "open": records[0].open
-          }
-          tradeDicts.forEach(trade => {
-            if (pair.from.isNative()) {
-              if (trade.base_asset_type === 'native' && trade.counter_asset_code === pair.to.code && trade.counter_asset_issuer === pair.to.issuer) {
-                tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
-                tmp['stat']['latest_price'] = trade.price.n / trade.price.d
-              }
-            } else if (pair.to.isNative()) {
-              if (trade.counter_asset_type === 'native' && trade.base_asset_code === pair.from.code && trade.base_asset_issuer === pair.from.issuer) {
-                tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
-                tmp['stat']['latest_price'] = trade.price.n / trade.price.d
-              }
-            } else {
-              if (trade.counter_asset_code === pair.to.code && trade.counter_asset_issuer === pair.to.issuer && trade.base_asset_code === pair.from.code && trade.base_asset_issuer === pair.from.issuer) {
-                tmp['stat']['latest_price_xcn'] = trade.price.n / trade.price.d
-                tmp['stat']['latest_price'] = trade.price.n / trade.price.d
-              }
-            }
-          })
-
-        } else {
-          tmp['stat'] = {
-            "latest_price_xcn": "0",
-            "low": "0",
-            "base_volume": "0",
-            "close": "0",
-            "order_book_avg": "0",
-            "latest_price": "0",
-            "counter_volume": "0",
-            "high": "0",
-            "open": "0"
-          }
-        }
-        if (allResp[pair.to.code] == undefined) {
-          allResp[pair.to.code] = []
-        }
-        allResp[pair.to.code].push(tmp)
-      })
-      return allResp
-    })
-    .catch(err => {
-      return err
-    })
-
-    // StellarServer.trades()
-    //   .forAssetPair(baseAsset, counterAsset)
-    //   .limit(1)
-    //   .order('desc')
-    //   .call()
-    //   .then(r => {
-    //     console.log('trades', r.records[0].price)
-    //   })
-    //   .catch(err => {
-    //     console.log(err)
-    //   })
-
+    return await traidPairCache.get('pair');
   }
 });
 
